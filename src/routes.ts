f2d88ec6687e9042a7a7d4bf361c9c1930ce6179
saadbaigsalsoft6252 <saadbaig200@gmail.com>
2023-05-25 02:11:07 +0500

    pagination donegit add .! server is now ready

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
-import { Express } from "express"
+import { Express } from "express";
 import todoRouter from "./Routes/todo";
-import userRouter from "./Routes/user"
+import userRouter from "./Routes/user";
 
 function routes(app: Express) {
     app.use("/api/todo", todoRouter)
